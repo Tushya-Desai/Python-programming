0fb74ff113ee51690217f954966fea35f5a853af
Tushya-Desai <tushya1401@gmail.com>
2025-03-25 06:25:18 +0530

    Delete Lab 7 directory

--- Lab 7/Program 1.py
+++
@@ -1,8 +0,0 @@
-l=[]
-n=int(input("How many elements you want to enter :"))
-for x in range(n):
-    k=input("Enter element :")
-    l.append(k)
-print("The original list =",l)
-s={i.upper() for i in l}
-print("The set =",s)
