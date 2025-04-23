95bc0bf56dbf8da7af507399404d224159320818
Tushya-Desai <tushya1401@gmail.com>
2025-04-23 08:44:26 +0530

    Delete Lab 9a/Lab 11 directory

--- Lab 9a/Lab 11/Program 1.py
+++
@@ -1,8 +0,0 @@
-while(True):
-    try:
-        a=int(input("Enter a number:"))
-        break
-    except ValueError:
-        print("It is not a integer")
-print("The number you entered is =",a)
-        
