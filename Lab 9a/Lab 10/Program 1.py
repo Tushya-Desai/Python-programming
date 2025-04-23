6293a44f9f39c86d201badee03152582a2a60baf
Tushya-Desai <tushya1401@gmail.com>
2025-04-23 08:44:14 +0530

    Delete Lab 9a/Lab 10 directory

--- Lab 9a/Lab 10/Program 1.py
+++
@@ -1,3 +0,0 @@
-fp=open("C:\Semester 2\Computer(Python)\Lab 11\Myfile.csv",'w')
-fp.write("Hello World")
-fp.close()
--- Lab 9a/Lab 10/Program 4.py
+++
@@ -1,4 +0,0 @@
-fp=open("C:\Semester 2\Computer(Python)\Lab 10\Myfile4.txt",'w')
-fp.write("Hello world \n")
-fp.write("Welcome to world of coding")
-fp.close()
